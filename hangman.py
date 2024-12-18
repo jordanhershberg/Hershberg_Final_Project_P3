edf70a85f65fa47dc76b011a30fbad6a47942239
Jordan Hershberg <maj1hershberg@gmail.com>
2024-12-18 04:47:21 +0000

    edited it a little

--- hangman.py
+++ hangman.py
@@ -1,11 +1,4 @@
 def create_hangman(wrong_guesses):
-  """
-  Creates the hangman graphic based on wrong guesses.
-  Args:
-    wrong_guesses: Number of incorrect guesses.
-  Returns:
-    A list representing the hangman graphic.
-  """
   hangman_parts = [
     ["head", "neck", "torso", "left arm", "right arm", "left leg", "right leg", "left sonic screwdriver", "right sonic screwdriver"],
     ["head", "neck", "torso", "left arm", "right arm", "left leg", "right leg", "left sonic screwdriver"],
@@ -17,41 +10,43 @@ def create_hangman(wrong_guesses):
     ["head", "neck"],
     ["head"]
   ]
-  
   return hangman_parts[wrong_guesses]
+
+
+
+
+
+
 def play_hangman():
-  """
-  Plays the hangman game.
-  """
-  word = input("Enter a word for the player to guess: ")
+  word = input("enter a word for the player to guess: \n ")
   print("\033[H\033[J", end="")
   word_letters = set(word)
   alphabet = set(chr(i) for i in range(97, 123))
   used_letters = set()
   lives = 9
-  # Get user input
+
   while len(word_letters) > 0 and lives > 0:
-    print(f"You have {lives} lives left.")
-    print(f"You have used these letters: {' '.join(used_letters)}")
-    # Current word (ie W - R D)
+    print("you have " + str(lives) + " lives left.")
+    print("you have used these letters: " + ",".join(used_letters))
+
     word_list = [letter if letter in used_letters else '_' for letter in word]
-    print('Current word: ', ' '.join(word_list))
-    user_letter = input('Guess a letter: ').lower()
+    print('current word: ', ' '.join(word_list))
+    user_letter = input('guess a letter: \n ')
     if user_letter in alphabet - used_letters:
       used_letters.add(user_letter)
       if user_letter in word_letters:
         word_letters.remove(user_letter)
         print('')
       else:
-        lives -= 1  # Takes away a life if wrong
-        print(f'Letter is not in word. {create_hangman(lives)}')
+        lives = lives-1
+        print("Letter is not in word." + str(create_hangman(lives)))
     elif user_letter in used_letters:
-      print('You have already used that character. Please try again.')
+      print("you already used that character, try again :/")
     else:
-      print('Invalid character. Please try again.')
-  # Gets here when len(word_letters) == 0 OR lives == 0
+      print("invalid character, only use individual letters, lowercase, no spaces")
+
   if lives == 0:
-    print(f'You died, sorry. The word was {word}')
+    print('hangman got hanged :( the word was ' + word)
   else:
-    print(f'YAY! You guessed the word {word}!!')
+    print('you guessed right, the word was ' + word)
 play_hangman()
